97b6e09edfa7324ed1f7079fba0876887f4b1263
TrymVei <tve@variant.no>
2024-06-18 14:38:42 +0200

    feat: hook for form input

---
+++ src/hooks/useFormInput.ts
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+function useFormInput(initialValue: string) {
+  const [value, setValue] = useState(initialValue);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  return {
+    value,
+    onChange: handleChange,
+  };
+}
+
+export default useFormInput;
