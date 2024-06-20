bea6e5d9df3533ffe14a536baa2ffd1e966a19ed
Trym Veiby <110395932+TrymVei@users.noreply.github.com>
2024-06-20 12:24:58 +0200

    feat: center text component with new section (#6)
    * feat: center text component with new section
    
    * feat: deleted old styling for hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-import ClimateTax from "./components/climateTax/ClimateTax";
-import Emissions from "./components/emissions/Emissions";
-import CompanyInfoForm from "./components/forms/CompanyInfoForm";
-import Hero from "./components/hero/Hero";
+import CenterText from './components/centerText/CenterText';
+import ClimateTax from './components/climateTax/ClimateTax';
+import Emissions from './components/emissions/Emissions';
+import CompanyInfoForm from './components/forms/CompanyInfoForm';
+import Hero from './components/hero/Hero';
 
 export default function Home() {
   return (
@@ -11,6 +12,13 @@ export default function Home() {
         <CompanyInfoForm />
         <ClimateTax />
         <Emissions />
+        <CenterText>
+          <h2>Veien fremover</h2>
+          Uten å gjøre tiltak for å senke utslippene sine, går ofte vekst i
+          selskapet og utslipp hånd i hånd. Se hvordan utslippet ditt øker med
+          høyere omsetning, og hva som skjer om du gjør tiltak for å senke
+          utslippene dine.
+        </CenterText>
       </div>
     </main>
   );
